c35db3fdfd2f5255f921f1fa43c678cbe36efc21
Javlon <59303287+javlona@users.noreply.github.com>
2021-12-24 19:28:58 -0600

    ....

---
+++ app.js
@@ -0,0 +1,6 @@
+const userName = document.querySelector('#firstName')
+const phone = document.querySelector('#phone')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const passwordConfirmation = document.querySelector('#passwordConfirmation')
+
